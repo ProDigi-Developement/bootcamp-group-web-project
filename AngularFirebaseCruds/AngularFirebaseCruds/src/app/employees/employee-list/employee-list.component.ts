d282c2013c68b078c83fbcb03b1f006801045cb0
Akshaykumar12 <akkipaladiya@gmail.com>
2018-06-13 15:44:01 -0300

    Display Employee List
    Get the List of Employees from the firebase, and dispaly it on employee list component.

--- AngularFirebaseCruds/AngularFirebaseCruds/src/app/employees/employee-list/employee-list.component.ts
+++ AngularFirebaseCruds/AngularFirebaseCruds/src/app/employees/employee-list/employee-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,11 +9,20 @@ import { EmployeeService } from '../shared/employee.service';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
+ employeeList:Employee[];
   constructor(private employeeService:EmployeeService) { }
 
   ngOnInit() {
+    var x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item=>{
+      this.employeeList = [];
+      item.forEach(element=>{
+        var y = element.payload.toJSON();
+        y["key"] = element.key;
+        this.employeeList.push(y as Employee);
+      });
+    });
   }
-  
+
 
 }
