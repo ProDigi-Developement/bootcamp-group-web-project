fb799f1a9de74a01e5362e85df16ee3df600b231
Alan <alansebastian4@gmail.com>
2018-07-20 10:13:54 -0400

    Called API to get number of people currently in space

--- Students-Projects/lan/js/script.js
+++ Students-Projects/lan/js/script.js
@@ -2,7 +2,14 @@
 
 function setup( ){
 
+	
+	fetch("http://api.open-notify.org/astros.json")
+  .then( (resp)=> resp.json())
+  .then( function (data){
+    let count = data.number;
 
+    
+    document.getElementById("result").innerHTML = count;
 
 
 
