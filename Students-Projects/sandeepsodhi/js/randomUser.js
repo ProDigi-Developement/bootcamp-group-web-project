40fa3762f340005e5baf3cc58d587cee7f3f7ff0
Sandeep Sodhi <sandeepsodhi@github.com>
2018-07-25 12:10:05 -0400

    adding javascript and userIds in html

--- Students-Projects/sandeepsodhi/js/randomUser.js
+++ Students-Projects/sandeepsodhi/js/randomUser.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
 });   
 
+function search(){
+    var nPerson = $("#nPersons").val();
+    var gender = $("#gender").val();
+    $("#data").show();
+    $.ajax({
+        "url": "https://randomuser.me/api?results="+nPerson+"&gender="+gender,
+            "dataType": "json",
+            "success": function(data) {
+                $.each(data.results, function (index, client) {
+                    $cardItem = "<div class='col-sm-3' >";
+                    $cardItem +=    "<div class='card'>";
+                    $cardItem +=        "<canvas class='header-bg' width='250' height='70' id='header-blur'></canvas>";
+                    $cardItem +=        "<div class='avatar'><img src='" + client.picture.large  + "'></div>";
+                    $cardItem +=        "<div class='content'><p>" + client.name.first + ' ' + client.name.last + "</p></div>";
+                    $cardItem +=    "</div>";
+                    $cardItem += "</div>";
+                    $('#row').append($cardItem);
+                });
+            },
+            "error": function(request, status, error) {
+                handleError(error);
+            }
+        });
+}
